f8873141e96669eb161b57003dc9b855b33df74b
Raick Staine <raickstaine@gmail.com>
2025-03-19 16:52:54 -0300

    Começando o projeto backend-express

---
+++ src/routers/userRouter.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import listUserController from '../controllers/user/listUserController.js'
+import getUserController from '../controllers/user/getUserController.js'
+import createUserController from '../controllers/user/createUserController.js'
+import updateUserController from '../controllers/user/updateUserController.js'
+import deleteUserController from '../controllers/user/deleteUserController.js'
+import editNameUserController from '../controllers/user/editNameUserController.js'
+
+const router = express.Router()
+
+router.get('/list', listUserController)
+router.get('/:id', getUserController)
+router.post('/', createUserController)
+router.put('/:id', updateUserController)
+router.delete('/:id', deleteUserController)
+router.patch('/nome/:id', editNameUserController)
+
+export default router
