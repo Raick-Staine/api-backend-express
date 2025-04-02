44b6c6df705121022e011346a258adad130710d1
Raick Staine <raickstaine@gmail.com>
2025-04-02 15:54:06 -0300

    aditando o edit\nameProductsController.js

--- src/controllers/products/editNameProductsController.js
+++ src/controllers/products/editNameProductsController.js
@@ -1,19 +1,34 @@
-import { updateName } from "../../models/productsModel.js"
+import { updateName } from "../../models/productsModel.js";
 
-export default async function(req, res) {
-    const { id } = req.params
-    const {name} = req.body
+export default async function (req, res) {
+    const { id } = req.params;
+    const { quantidade } = req.body;
 
-    const result = await updateName(+id, name)
-
-    if(!result){
-        return res.status(404).json({
-            error: 'Produto não encontrado'
-        })
+    // Converte quantidade para número inteiro
+    const parsedQuantidade = parseInt(quantidade, 10);
+    if (isNaN(parsedQuantidade)) {
+        return res.status(400).json({
+            error: "O valor de quantidade é inválido. Deve ser um número inteiro.",
+        });
     }
 
-    return res.json({
-        message: "Nome do produto atualizado com sucesso",
-        product: result
-    })
+    try {
+        const result = await updateName(+id, parsedQuantidade);
+
+        if (!result) {
+            return res.status(404).json({
+                error: "Produto não encontrado",
+            });
+        }
+
+        return res.json({
+            message: "Quantidade do produto atualizada com sucesso",
+            product: result,
+        });
+    } catch (error) {
+        console.error("Erro ao atualizar a quantidade do produto:", error.message);
+        return res.status(500).json({
+            error: "Erro interno no servidor",
+        });
+    }
 }
